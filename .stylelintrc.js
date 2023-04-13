d15cb1c82a4ec75cab7600d69c53cfc43e5e3100
Flavio Castro <flaviocastro2013@gmail.com>
2023-04-13 17:33:51 +0100

    Update @untile/stylelint-config-react to v1.0.2
    This allows the deprecated stylelint v15 rules to be dumped.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,8 +3,5 @@
  */
 
 module.exports = {
-  extends: ['@untile/stylelint-config-react'],
-  rules: {
-    'declaration-block-semicolon-space-after': 'always-single-line'
-  }
+  extends: ['@untile/stylelint-config-react']
 };
